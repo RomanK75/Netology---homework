4b8d70a21e3820eedb57b9432eb0e4a09634d38e
Mytth <Mytth75@gmail.com>
2023-02-18 22:57:42 +0700

    first_task_completed

--- quest_1.py
+++ quest_1.py
@@ -6,6 +6,14 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+    def lecture_rate(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.lecturer_rate:
+                lecturer.lecturer_rate[course] += grade
+            else:
+                lecturer.lecturer_rate[course] = grade
+        else:
+            print('Error')
         
 class Mentor:
     def __init__(self, name, surname):
@@ -13,27 +21,37 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
         
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
-# class Lecture(Mentor):
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.lecturer_rate = {}
 
-# class Reviewer(Mentor):
+class Reviewer(Mentor):
+        def rate_hw(self, student, course, grade):
+            if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+                if course in student.grades:
+                    student.grades[course] += [grade]
+                else:
+                    student.grades[course] = [grade]
+            else:
+                return 'Ошибка'
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
-print(cool_mentor.courses_attached)
- 
+
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
+
+test_lector = Lecturer('Any','Pypkin')
+test_lector.courses_attached += ['Python']
+best_student.lecture_rate(test_lector, 'Python', 5 )
+
+print(test_lector.lecturer_rate)
+best_student.lecture_rate(test_lector, 'Python', 5 )
+print(test_lector.lecturer_rate)
  
 print(best_student.grades)
